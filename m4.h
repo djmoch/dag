c1ee6b82af949ffecb70f25d55fece1dc90903c3
Daniel Moch <daniel@danielmoch.com>
2020-09-02 12:19:52 -0400

    create separate module for m4

---
+++ m4.h
@@ -0,0 +1,8 @@
+/* See LICENSE file for copyright and license details */
+#ifdef __m4_h
+#define __m4_h
+
+char * make_m4_cmd(const char **defines, const int num_defines,
+	const char *infile, const char *outfile);
+
+#endif
