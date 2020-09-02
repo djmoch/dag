18f32a9be40d81ff0a3de609aba0629fae70af48
Daniel Moch <daniel@danielmoch.com>
2020-09-02 11:28:08 -0400

    string.h: Add guard statements

--- string.h
+++ string.h
@@ -4,8 +4,13 @@
  * unless otherwise specified
  */
 
+#ifndef __DAG_STRING_H
+#define __DAG_STRING_H
+
 int strbegin(const char *big, const char *little);
 char *strnswp(char *big, const char *old, const char *new, size_t max);
 
 /* assumes the last element of strarray is NULL */
 void dedup(char **strarray);
+
+#endif
